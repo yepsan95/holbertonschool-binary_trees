4dac5efd1bc6717b11d6302a191caf59d1f88b4e
Bryan Yep <yepsan95@gmail.com>
2023-05-17 19:00:49 -0700

    feat: added function that goes through a binary tree using pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -16,6 +16,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 
 	func(tree->n);
 
-	binary_tree_preorder(tree->left, (*func)(int));
-	binary_tree_preorder(tree->right, (*func)(int));
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
