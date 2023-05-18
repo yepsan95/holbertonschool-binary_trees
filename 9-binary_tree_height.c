57ea7a93e7b5ecf4fd97c25ab29796d2864c2db9
Bryan Yep <yepsan95@gmail.com>
2023-05-17 23:05:02 -0700

    fix: recursion stops at the root node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,9 +12,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
-	if (tree->parent == NULL)
-		return (0);
 
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
