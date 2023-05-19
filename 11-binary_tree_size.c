eb9cfc5b2da145abb6a86df28225ec1ce3acf9c8
Bryan Yep <yepsan95@gmail.com>
2023-05-18 20:05:49 -0700

    feat: added function that measures the size of a binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,7 +11,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t size = 1;
 
 	if (tree == NULL)
-		return;
+		return (0);
 
 	binary_tree_size(tree->left);
 	binary_tree_size(tree->right);
