a72d8ec7fd760471e8876c19f71f08eb856ed59a
Bryan Yep <yepsan95@gmail.com>
2023-05-18 19:41:49 -0700

    fix: count starts at 0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,6 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
 
