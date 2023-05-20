f5aca1e57352c9ab1ed371b2bda11af1ac7dcc8a
Bryan Yep <yepsan95@gmail.com>
2023-05-20 09:35:19 -0700

    fix: depth starsts now at 0

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,7 +10,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
 
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
 
 	depth = binary_tree_depth(tree->parent) + 1;
