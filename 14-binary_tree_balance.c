caca3d22e97a42f10048532e88cb096d84ce7a3a
Bryan Yep <yepsan95@gmail.com>
2023-05-20 14:14:36 -0700

    fix: handled case when root->left or root->right is NULL

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,6 +14,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	bf = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+	if (tree->left == NULL)
+		bf = binary_tree_height(tree) * -1;
+	else if (tree->right == NULL)
+		bf = binary_tree_height(tree);
 
 	return (bf);
 }
