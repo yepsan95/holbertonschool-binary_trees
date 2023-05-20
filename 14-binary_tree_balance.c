74c34859897cc30e81259dbce73c4a6d7b62ed60
Bryan Yep <yepsan95@gmail.com>
2023-05-20 12:30:02 -0700

    feat: added binary_tree_height function in file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,3 +17,28 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (bf);
 }
+
+/**
+ * binary_tree_height - measures the heigth of a binary tree
+ * @tree: pointer to the root node of the tree to traverse
+ *
+ * Return: height of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int l_height, r_height;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
+
+	if (l_height > r_height)
+		return (l_height + 1);
+	else
+		return (r_height + 1);
+}
